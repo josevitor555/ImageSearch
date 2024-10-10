00dd6ff1b9a5231c9b27ec10cd9f796e71e8c731
josevitor555 <koservitor123@gmail.com>
2024-10-10 18:45:24 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import dotenv from "dotenv";
+
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
